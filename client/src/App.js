2f850d30026d01f46090f6e2f836f6a187e7c6f6
sdenu9564 <smanna100@gmail.com>
2024-01-04 01:06:14 +0530

    account added

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import Messanger from "./component/Messanger";
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import AccountProvider from "./context/accountProvider";
 function App() {
-  const clientId = '164153058455-ck8ba5t3gg0ro5uqj7biu84el2r6qm50.apps.googleusercontent.com'
+  const clientId = ''
   return (
    <GoogleOAuthProvider clientId={clientId}>
     <AccountProvider>
